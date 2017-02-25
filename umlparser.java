5a944aa346f07f227a4f9ded61f600e953c34823
ipsha1545 <ipsha1545@gmail.com>
2017-02-24 23:19:45 -0800

    Modified uml.parser.
    Modified uml.parser.

--- umlparser.java
+++ umlparser.java
@@ -1,87 +1,42 @@
- import java.io.BufferedInputStream;
-	import java.io.ByteArrayOutputStream;
-	import java.io.File;
-	import java.io.FileInputStream;
-	import java.io.FileOutputStream;
-	import java.io.InputStream;
-	import java.io.InputStreamReader;
-	import java.net.URISyntaxException;
-	import java.net.URL;
-	import java.util.ArrayList;
-	import java.util.HashMap;
-	import java.util.List;
-	import java.util.concurrent.ConcurrentHashMap;
+ import java.io.File;
+import java.util.ArrayList;
 
-	import com.github.javaparser.JavaParser;
-	import com.github.javaparser.ast.CompilationUnit;
-	import com.github.javaparser.ast.Node;
-	import com.github.javaparser.ast.body.BodyDeclaration;
-	import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-	import com.github.javaparser.ast.body.ConstructorDeclaration;
-	import com.github.javaparser.ast.body.FieldDeclaration;
-	import com.github.javaparser.ast.body.MethodDeclaration;
-	import com.github.javaparser.ast.body.ModifierSet;
-	import com.github.javaparser.ast.body.Parameter;
-	import com.github.javaparser.ast.body.TypeDeclaration;
-	import com.github.javaparser.ast.body.VariableDeclarator;
-	import com.github.javaparser.ast.body.VariableDeclaratorId;
-	import com.github.javaparser.ast.type.ClassOrInterfaceType;
-	import com.github.javaparser.ast.type.PrimitiveType;
-	import com.github.javaparser.ast.type.ReferenceType;
-	import com.github.javaparser.ast.type.VoidType;
 
-	public class Umlparser {
+public class Umlparser {
+	private String inputfilepath = null;
+	private static File container = null;
+	private static File[] allfiles = null;
+	private StringBuilder URL = new StringBuilder();
+	private ArrayList<String> attributeList = new ArrayList<String>();
+	private ArrayList<String> functionList = new ArrayList<String>();
+	private ArrayList<String> constructorList = new ArrayList<String>();
+	
 		
-		//set file path according to the args
-		private static String filePath = null;// "D:/projects/UMLJavaParser/src/javaparser";
-		private static File folder = null;//new File(filePath+"/"+"testcase5");
-		private static File[] listOfFiles = null;//folder.listFiles();
-		private StringBuilder bodyURL = new StringBuilder();
-		private ArrayList<String> interfaceList = new ArrayList<String>();
-		private ArrayList<String> variableList = new ArrayList<String>();
-		private ArrayList<String> methodParamList = new ArrayList<String>();
-		private ArrayList<String> methodList = new ArrayList<String>();
-		private ArrayList<String> constructList = new ArrayList<String>(); 
-		private ArrayList<String> constructParamList = new ArrayList<String>();
-		private HashMap<String,List<ClassOrInterfaceType>> classInterfaceMap = new HashMap<String,List<ClassOrInterfaceType>>();
-		private HashMap<String,List<ClassOrInterfaceType>> classSuperClassMap = new HashMap<String,List<ClassOrInterfaceType>>();
-		private String className = "";
-		private String[] primitives = {"byte","short","int","long","float","double","boolean","char",
-				"Byte","Short","Integer","Long","Float","Double","Boolean","Char"};
-		private ConcurrentHashMap<String,String> multiplicityMap = new ConcurrentHashMap<String,String>();
-		private ConcurrentHashMap<String,String> usesMap = new ConcurrentHashMap<String,String>();
-		private ConcurrentHashMap<String,String> usessMap = new ConcurrentHashMap<String,String>();
-		private String usesRelation="";
-		//private boolean flag = true;
-		private String usesInterface="";
-		boolean flag=true;
-		public Umlparser()
-		{
-		}
-		public static void main(String[] args) throws URISyntaxException
-		{
-			//if(args.length == 2)
-			//{
-				//String path = args[0];
-				//String newFileName = args[1];
-				String path = "C:\\Users\\ipsha\\workspace\\Testingclasses\\src";
-				String newFileName = "C:\\Users\\ipsha\\workspace\\Testingclasses\\src\\abcd.png";
-				
-			folder = new File(path);
-			//path = "../"+path+"/"; 
-			//System.out.println("New path"+path);
-			//System.out.println(Java2UMLParser.class.getResource("/").getFile());
-			//folder = new File(Java2UMLParser.class.getResource("..\testcase1\\").getFile());
-			//InputStreamReader in = new InputStreamReader(Java2UMLParser.class.getResourceAsStream(path));
-			//folder = new File(in.get);
-			listOfFiles = folder.listFiles();
-			Umlparser obj = new Umlparser();
+	
+	public static void main(String[] args) {
+		String inputfilename = "C:\\Users\\ipsha\\workspace\\Testingclasses\\src";
+		String outputfilename = inputfilename + "\\" + "neww.png";
+		container = new File(inputfilename);
+		allfiles = container.listFiles();
+		Umlparser myobject = new Umlparser();
+		myobject.parseit(outputfilename);
+	}
+	
+	
+	public String parseit(String image){
+		
+		for(File file : allfiles){
 			
-			//}
-			//else//
-			//{
-				//System.out.println("Provide all required arguments.");
-			//}
-		}	
-
+			if(URL.length() > 0 && (URL.charAt(URL.length()-1) != ','))
+			{
+				URL.append(",");
+			}
+			attributeList = new ArrayList<String>();
+			functionList = new ArrayList<String>();
+			constructorList = new ArrayList<String>();
+			URL.append("[");
+		}
+		return null;
+	}
+	
 }
