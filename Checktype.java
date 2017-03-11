c866f656305d8cd950f96146f89aa06b6790339c
ipsha1545 <ipsha1545@gmail.com>
2017-03-10 23:08:05 -0800

    Checked for access modifiers
    Created  a new file and checked for access modifiers

--- Checktype.java
+++ Checktype.java
@@ -1,1 +1,38 @@
+List<TypeDeclaration> Types = unitc.getTypes();
+					for(TypeDeclaration anyonetype : Types)
+					{
+						List<BodyDeclaration> mydec = bodyType.getMembers();
+						
+						if(!mydec.isEmpty() && mydec.size()>0)
+						{
+							String a = "";
+							for(BodyDeclaration body : medic)
+							{
+								if(body instanceof FieldDeclaration)
+								{
+									String p="";
+									FieldDeclaration fidec = (FieldDeclaration)body;
+									
+									int f = fieldDec.getModifiers();
+									boolean doesexist = false;
 
+									switch(f)
+									{
+									case ModifierSet.PRIVATE:
+										a = "-";
+										doesexist= true;
+										break;
+									case ModifierSet.PUBLIC:
+										a= "+";
+										doesexist= true;
+										break;
+									
+									}
+
+								}
+                
+						 }		
+                        
+           }
+            
+         }    
