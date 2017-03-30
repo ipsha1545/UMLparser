8800077a0b27afe395eeaf089a5cad00e17f10ab
ipsha1545 <ipsha1545@gmail.com>
2017-03-30 08:22:11 -0700

    Checked types
    Checked types and added contents of  checktype.java

--- umlparser.java
+++ umlparser.java
@@ -70,6 +70,177 @@ public class Umlparser {
 							
 						}
 					}
+							List<TypeDeclaration> Types = unitc.getTypes();
+						for(TypeDeclaration anyonetype : Types)
+						{
+							List<BodyDeclaration> mydec = bodyType.getMembers();
+							
+							if(!mydec.isEmpty() && mydec.size()>0)
+							{
+								String a = "";
+								for(BodyDeclaration body : medic)
+								{
+									if(body instanceof FieldDeclaration)
+									{
+										String p="";
+										FieldDeclaration fidec = (FieldDeclaration)body;
+										
+										int f = fidec.getModifiers();
+										boolean doesexist = false;
+
+
+										switch(f)
+										{
+										case ModifierSet.PRIVATE:
+											a = "-";
+											doesexist= true;
+											break;
+										case ModifierSet.PUBLIC:
+											a= "+";
+											doesexist= true;
+											break;
+										
+										}
+										
+										if(doesexist)
+										{
+											boolean newdoesexist = true;
+
+
+											
+											
+											List<Node> f = fieldDec.getChildrenNodes();
+											
+											for(Node Nodes :f)
+											{
+												if(Nodes instanceof ReferenceType)
+												{
+													String refType = ((ReferenceType) Nodes).getType().toString();
+													if(refType.equals("String"))
+													{
+														primitiveType += refType;
+													}
+													else
+													{
+														boolean foundPrimitive = false;
+														boolean foundCollection = false;
+
+
+														for(String primitiveRef : primitives)
+														{
+															if(refType.contains(primitiveRef))
+															{
+																primitiveType += refType+"(*)";
+																foundPrimitive = true;
+																break;
+															}
+														}
+														if(!foundPrimitive)
+														{
+															// logic for checking multiplicity
+															if(checkForMultiplicity(refType,className))
+															{
+																newdoesexist = false;
+																foundCollection = true;
+																break;
+															}
+															
+														}
+													}
+												}
+												else if(Nodes instanceof PrimitiveType)
+												{
+													PrimitiveType pType = (PrimitiveType)Nodes;
+													primitiveType = pType.toString();
+												}
+												if(Nodes instanceof VariableDeclarator && newdoesexist)
+												{
+													VariableDeclarator variableDec = (VariableDeclarator)Nodes;
+													variableList.add(accessModifier+variableDec.toString()+":"+primitiveType);
+												}
+											}
+									    }
+										
+									}
+									
+									else if(body instanceof MethodDeclaration)
+									{
+										if(istrue==true){
+										String temporary = "";
+										MethodDeclaration method = (MethodDeclaration)body;
+										String ma = "";
+										String mr = "";
+										String mN = "";
+										int m = method.getModifiers();
+										boolean doesnotexist = false;
+
+
+										switch(m)
+										{
+										case ModifierSet.PUBLIC:
+											ma = "+";
+											doesnotexist = true;
+											break;
+										case ModifierSet.PUBLIC+ModifierSet.STATIC:
+											ma = "+";
+											doesnotexist = true;
+											break;
+									    }
+										if(doesexist)
+										 {
+											List<Node> mynodechild =   method.getChildrenNodes();
+
+
+											for(Node eachchildnode : mynodechild)
+											{
+												if(eachchildnode instanceof ReferenceType)
+												{
+													ReferenceType rM = (ReferenceType)eachchildnode;
+
+
+													methodRefType = rM.getType().toString();
+												}
+												else if(eachchildnode instanceof VoidType)
+												{
+													methodRefType = "void";
+												}
+											}
+											Name  = method.getName();
+		
+											List<Parameter Param = method.getParameters();
+
+
+											ListofParam = new ArrayList<String>();
+
+
+											if(Param.size() > 0)
+											{
+												tempMethodParam += "(";
+												for(Parameter eachparam : Param)
+												{
+													List<Node> Childparam = param.getChildrenNodes();
+
+
+													String methodParamReferenceType="", variable="";
+
+
+											}
+
+
+										}
+	  								}
+
+
+							       }
+	                
+					        	}		
+				            
+				             }
+	                           
+							
+				}
+	                }
+	        
 		}
 		return null;
 	}
