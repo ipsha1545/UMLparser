9f074bd33d474f9e66f8cc2c9383745d64698fa8
ipsha1545 <ipsha1545@gmail.com>
2017-03-31 21:57:40 -0700

    Checked dependency for children Nodes
    Checked dependency for children Nodes

--- umlparser.java
+++ umlparser.java
@@ -26,221 +26,235 @@ public class Umlparser {
 	public String parseit(String image)
 	{
 		
-		for(File file : allfiles){
-			
-			if(URL.length() > 0 && (URL.charAt(URL.length()-1) != ','))
+	for(File file : allfiles){
+
+	if(URL.length() > 0 && (URL.charAt(URL.length()-1) != ','))
+	{
+		URL.append(",");
+	}
+	attributeList = new ArrayList<String>();
+	functionList = new ArrayList<String>();
+	constructorList = new ArrayList<String>();
+	URL.append("[");
+	FileInputStream inputStream = new FileInputStream(folder.toString()+"/"+file.getName().split("\\.")[0]+".java");
+	CompilationUnit unitc = JavaParser.parse(inputStream);
+	List<Node> childrenNodes = unitc.getChildrenNodes();
+	for(Node eachchild : childrenNodes)
+	{
+	if(eachchild instanceof ClassOrInterfaceDeclaration)
+	{
+		ClassOrInterfaceDeclaration cIDec = (ClassOrInterfaceDeclaration)eachchild;
+	if(cIDec.isInterface())
+	{
+		interfaceList.add(cIDec.getName());
+		URL.append("<<Interface>>;");
+		URL.append(cIDec.getName());
+		URL.append("");
+		flag=false;
+		continue;
+	}
+	flag=true;
+	URL.append(cIDec.getName());
+	className = cIDec.getName();
+	List<ClassOrInterfaceType> implementsList = cIDec.getImplements();
+	if(implementsList!=null)
+	{
+		classInterfaceMap.put(classInterfaceDec.getName(), implementsList);
+	}
+
+	List<ClassOrInterfaceType> extendsList = cIDec.getExtends();
+	if(extendsList!=null)
+	{
+		classSuperClassMap.put(cIDec.getName(), extendsList);
+	}
+
+}
+}
+	List<TypeDeclaration> Types = unitc.getTypes();
+for(TypeDeclaration anyonetype : Types)
+{
+	List<BodyDeclaration> mydec = bodyType.getMembers();
+
+	if(!mydec.isEmpty() && mydec.size()>0)
+	{
+		String a = "";
+		for(BodyDeclaration body : medic)
+		{
+			if(body instanceof FieldDeclaration)
 			{
-				URL.append(",");
-			}
-			attributeList = new ArrayList<String>();
-			functionList = new ArrayList<String>();
-			constructorList = new ArrayList<String>();
-			URL.append("[");
-			FileInputStream inputStream = new FileInputStream(folder.toString()+"/"+file.getName().split("\\.")[0]+".java");
-					CompilationUnit unitc = JavaParser.parse(inputStream);
-					List<Node> childrenNodes = unitc.getChildrenNodes();
-					for(Node eachchild : childrenNodes)
+				String p="";
+				FieldDeclaration fidec = (FieldDeclaration)body;
+
+				int f = fidec.getModifiers();
+				boolean doesexist = false;
+
+
+				switch(f)
+				{
+				case ModifierSet.PRIVATE:
+					a = "-";
+					doesexist= true;
+					break;
+				case ModifierSet.PUBLIC:
+					a= "+";
+					doesexist= true;
+					break;
+
+				}
+										
+				if(doesexist)
+				{
+					boolean newdoesexist = true;
+
+
+
+
+					List<Node> f = fieldDec.getChildrenNodes();
+
+					for(Node Nodes :f)
 					{
-						if(eachchild instanceof ClassOrInterfaceDeclaration)
+						if(Nodes instanceof ReferenceType)
 						{
-							ClassOrInterfaceDeclaration cIDec = (ClassOrInterfaceDeclaration)eachchild;
-						        if(cIDec.isInterface())
+							String refType = ((ReferenceType) Nodes).getType().toString();
+							if(refType.equals("String"))
 							{
-								interfaceList.add(cIDec.getName());
-								URL.append("<<Interface>>;");
-								URL.append(cIDec.getName());
-								URL.append("");
-								flag=false;
-								continue;
+								primitiveType += refType;
 							}
-							flag=true;
-							URL.append(cIDec.getName());
-							className = cIDec.getName();
-						        List<ClassOrInterfaceType> implementsList = cIDec.getImplements();
-							if(implementsList!=null)
+							else
 							{
-								classInterfaceMap.put(classInterfaceDec.getName(), implementsList);
+								boolean foundPrimitive = false;
+								boolean foundCollection = false;
+
+
+								for(String primitiveRef : primitives)
+								{
+									if(refType.contains(primitiveRef))
+									{
+										primitiveType += refType+"(*)";
+										foundPrimitive = true;
+										break;
+									}
+								}
+								if(!foundPrimitive)
+								{
+									// logic for checking multiplicity
+									if(checkForMultiplicity(refType,className))
+									{
+										newdoesexist = false;
+										foundCollection = true;
+										break;
+									}
+
+								}
 							}
-							
-							List<ClassOrInterfaceType> extendsList = cIDec.getExtends();
-							if(extendsList!=null)
-							{
-								classSuperClassMap.put(cIDec.getName(), extendsList);
-							}
-							
+						}
+						else if(Nodes instanceof PrimitiveType)
+						{
+							PrimitiveType pType = (PrimitiveType)Nodes;
+							primitiveType = pType.toString();
+						}
+						if(Nodes instanceof VariableDeclarator && newdoesexist)
+						{
+							VariableDeclarator variableDec = (VariableDeclarator)Nodes;
+							variableList.add(accessModifier+variableDec.toString()+":"+primitiveType);
 						}
 					}
-							List<TypeDeclaration> Types = unitc.getTypes();
-						for(TypeDeclaration anyonetype : Types)
+			    }
+
+			}
+
+			else if(body instanceof MethodDeclaration)
+			{
+				if(istrue==true){
+				String temporary = "";
+				MethodDeclaration method = (MethodDeclaration)body;
+				String ma = "";
+				String mr = "";
+				String mN = "";
+				int m = method.getModifiers();
+				boolean doesnotexist = false;
+
+
+				switch(m)
+				{
+				case ModifierSet.PUBLIC:
+					ma = "+";
+					doesnotexist = true;
+					break;
+				case ModifierSet.PUBLIC+ModifierSet.STATIC:
+					ma = "+";
+					doesnotexist = true;
+					break;
+			    }
+				if(doesexist)
+				 {
+					List<Node> mynodechild =   method.getChildrenNodes();
+
+
+					for(Node eachchildnode : mynodechild)
+					{
+						if(eachchildnode instanceof ReferenceType)
 						{
-							List<BodyDeclaration> mydec = bodyType.getMembers();
-							
-							if(!mydec.isEmpty() && mydec.size()>0)
-							{
-								String a = "";
-								for(BodyDeclaration body : medic)
-								{
-									if(body instanceof FieldDeclaration)
-									{
-										String p="";
-										FieldDeclaration fidec = (FieldDeclaration)body;
-										
-										int f = fidec.getModifiers();
-										boolean doesexist = false;
+							ReferenceType rM = (ReferenceType)eachchildnode;
 
 
-										switch(f)
-										{
-										case ModifierSet.PRIVATE:
-											a = "-";
-											doesexist= true;
-											break;
-										case ModifierSet.PUBLIC:
-											a= "+";
-											doesexist= true;
-											break;
-										
-										}
-										
-										if(doesexist)
-										{
-											boolean newdoesexist = true;
+							methodRefType = rM.getType().toString();
+						}
+						else if(eachchildnode instanceof VoidType)
+						{
+							methodRefType = "void";
+						}
+					}
+					Name  = method.getName();
+
+					List<Parameter Param = method.getParameters();
 
 
-											
-											
-											List<Node> f = fieldDec.getChildrenNodes();
-											
-											for(Node Nodes :f)
-											{
-												if(Nodes instanceof ReferenceType)
-												{
-													String refType = ((ReferenceType) Nodes).getType().toString();
-													if(refType.equals("String"))
-													{
-														primitiveType += refType;
-													}
-													else
-													{
-														boolean foundPrimitive = false;
-														boolean foundCollection = false;
+					ListofParam = new ArrayList<String>();
 
 
-														for(String primitiveRef : primitives)
-														{
-															if(refType.contains(primitiveRef))
-															{
-																primitiveType += refType+"(*)";
-																foundPrimitive = true;
-																break;
-															}
-														}
-														if(!foundPrimitive)
-														{
-															// logic for checking multiplicity
-															if(checkForMultiplicity(refType,className))
-															{
-																newdoesexist = false;
-																foundCollection = true;
-																break;
-															}
-															
-														}
-													}
-												}
-												else if(Nodes instanceof PrimitiveType)
-												{
-													PrimitiveType pType = (PrimitiveType)Nodes;
-													primitiveType = pType.toString();
-												}
-												if(Nodes instanceof VariableDeclarator && newdoesexist)
-												{
-													VariableDeclarator variableDec = (VariableDeclarator)Nodes;
-													variableList.add(accessModifier+variableDec.toString()+":"+primitiveType);
-												}
-											}
-									    }
-										
-									}
-									
-									else if(body instanceof MethodDeclaration)
-									{
-										if(istrue==true){
-										String temporary = "";
-										MethodDeclaration method = (MethodDeclaration)body;
-										String ma = "";
-										String mr = "";
-										String mN = "";
-										int m = method.getModifiers();
-										boolean doesnotexist = false;
+					if(Param.size() > 0)
+					{
+						tempMethodParam += "(";
+						for(Parameter eachparam : Param)
+						{
+				List<Node> Childparam = param.getChildrenNodes();
+				String methodParamReferenceType="", variable="";
+				
+				for(Node pch : Childparam)
+				{
+					if(pch instanceof VariableDeclaratorId)
+					{
+						VariableDeclaratorId v = (VariableDeclaratorId)paramChild;
+						variable = v.getName().toString();
+					}
+					else if(pch instanceof ReferenceType)
+					{
+						ReferenceType rtype = (ReferenceType)paramChild;
+						methodParamReferenceType = rtype.getType().toString();
+						CheckDependency cdep = new CheckDependency(className,method);
+						usesMap=cdep.checkDependency(interfaceList);
+				        }	
+
+			              }			
+
+                                   }
 
 
-										switch(m)
-										{
-										case ModifierSet.PUBLIC:
-											ma = "+";
-											doesnotexist = true;
-											break;
-										case ModifierSet.PUBLIC+ModifierSet.STATIC:
-											ma = "+";
-											doesnotexist = true;
-											break;
-									    }
-										if(doesexist)
-										 {
-											List<Node> mynodechild =   method.getChildrenNodes();
+				}
+	  	               }
 
 
-											for(Node eachchildnode : mynodechild)
-											{
-												if(eachchildnode instanceof ReferenceType)
-												{
-													ReferenceType rM = (ReferenceType)eachchildnode;
-
-
-													methodRefType = rM.getType().toString();
-												}
-												else if(eachchildnode instanceof VoidType)
-												{
-													methodRefType = "void";
-												}
-											}
-											Name  = method.getName();
-		
-											List<Parameter Param = method.getParameters();
-
-
-											ListofParam = new ArrayList<String>();
-
-
-											if(Param.size() > 0)
-											{
-												tempMethodParam += "(";
-												for(Parameter eachparam : Param)
-												{
-													List<Node> Childparam = param.getChildrenNodes();
-
-
-													String methodParamReferenceType="", variable="";
-
-
-											}
-
-
-										}
-	  								}
-
-
-							       }
+			    }
 	                
-					        	}		
+			  }		
 				            
-				             }
+	                }
 	                           
 							
-				}
-	                }
-	        
+		     }
+	           }
+	       
 		}
 		return null;
 	}
